29e393b3c801d059113475372f59f08acd845d34
Eleonore Euzenes <eleonore.euzenes@gmail.com>
2024-07-09 11:26:24 +0200

    CRUD Operations for Events and Data Validation

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import { createServer } from 'http';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import * as eventController from './controllers/eventController';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
 // Load environment variables from the specified .env file
 dotenv.config({ path: '../../.env' });
@@ -9,9 +12,20 @@ dotenv.config({ path: '../../.env' });
 const app = express();
 const httpServer = createServer(app);
 
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
+
+// Routes for event operations
+app.post('/api/event', eventController.createEvent);
+app.get('/api/events', eventController.getEvents);
+app.get('/api/events/:eventId', eventController.getEventById);
+app.put('/api/events/:eventId', eventController.updateEvent);
+app.delete('/api/events/:eventId', eventController.deleteEvent);
 
 // Ensure URL_MONGO is defined
 const dbUrl = process.env.URL_MONGO;
@@ -21,8 +35,8 @@ if (!dbUrl) {
 
 mongoose.connect(dbUrl).then(() => {
     console.log('Connected to MongoDB');
-    httpServer.listen(8081, () => {
-        console.log('listening on *:8081');
+    httpServer.listen(3000, () => {
+        console.log('listening on *:3000');
     });
 }).catch((error) => {
     console.log('Error connecting to MongoDB', error);
