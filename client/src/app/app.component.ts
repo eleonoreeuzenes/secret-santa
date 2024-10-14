d0cb275d5bea8d8ce377edd3de72b1618c88a7ed
Eleonore Euzenes <eleonore.euzenes@gmail.com>
2024-10-14 19:31:35 +0200

    forgot to rename one folder

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { CreateSecretSantaComponent } from './secret-santa/create-santa-event/create-secret-santa.component';
+import { CreateSecretSantaComponent } from './secret-santa/create-secret-santa/create-secret-santa.component';
 
 @Component({
   selector: 'app-root',
