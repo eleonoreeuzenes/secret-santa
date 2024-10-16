474443ac568808d776a9c7efa7568daf1db1d664
Eleonore Euzenes <eleonore.euzenes@gmail.com>
2024-10-16 14:02:27 +0200

    :truck: add routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { ShareSecretSantaComponent } from './secret-santa/share-secret-santa/share-secret-santa.component';
 import { CreateSecretSantaComponent } from './secret-santa/create-secret-santa/create-secret-santa.component';
 import { SeeSecretSantaComponent } from './secret-santa/see-secret-santa/see-secret-santa.component';
+import { ConfirmParticipantComponent } from './secret-santa/confirm-participant/confirm-participant.component';
+import { RevealAssignmentComponent } from './secret-santa/reveal-assignment/reveal-assignment.component';
 
 export const routes: Routes = [
   {
@@ -15,6 +17,16 @@ export const routes: Routes = [
   },
   {
     path: 'secretsanta/:secretSantaID',
-    component: SeeSecretSantaComponent
+    component: SeeSecretSantaComponent,
+    children: [
+      {
+        path: 'confirm-participant/:participantName',
+        component: ConfirmParticipantComponent,
+      },
+      {
+        path: 'reveal-assignment/:participantName',
+        component: RevealAssignmentComponent,
+      }
+    ]
   },
 ];
