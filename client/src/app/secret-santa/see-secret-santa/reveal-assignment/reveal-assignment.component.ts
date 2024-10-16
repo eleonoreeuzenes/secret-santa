6107a11e1038beb0185fff905e027826775f0bfa
Eleonore Euzenes <eleonore.euzenes@gmail.com>
2024-10-16 18:17:27 +0200

    add reveal name on button clicked

--- client/src/app/secret-santa/see-secret-santa/reveal-assignment/reveal-assignment.component.ts
+++ client/src/app/secret-santa/see-secret-santa/reveal-assignment/reveal-assignment.component.ts
@@ -2,28 +2,31 @@ import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SecretSantaService } from '../../secret-santa.service';
 import { GiftAssignment } from '../../git-assignement.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-reveal-assignment',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './reveal-assignment.component.html',
   styleUrl: './reveal-assignment.component.css'
 })
 export class RevealAssignmentComponent {
   participantName: string = '';
-  reciever: string | undefined = '';
+  reciever: string | undefined = 'Cliquez pour révéler le nom';
   CurrentGiftAssignement: GiftAssignment | undefined = undefined;
+  buttonClass: string = "bg-indigo-600 text-white hover:bg-indigo-800";
   private route = inject(ActivatedRoute);
   private SecretSantaService= inject(SecretSantaService);
 
   ngOnInit(): void {
     this.participantName = this.route.snapshot.paramMap.get('participantName')!;
     this.CurrentGiftAssignement = this.SecretSantaService.getSingleGiftAssignement(this.participantName);
-    this.reciever = this.CurrentGiftAssignement?.receiver;
+
   }
 
   revealReciever(participantName : string): void {
-    console.log("coucou"+ participantName)
+    this.reciever = this.CurrentGiftAssignement?.receiver;
+    this.buttonClass = "bg-white text-indigo-600 border border-slate-300"
   }
 }
