2b6ba979828697f9a45011bec4946b1f099cc5c0
Eleonore Euzenes <eleonore.euzenes@gmail.com>
2024-10-17 18:22:06 +0200

    animate pictures on header

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,27 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        moveDownMail: {
+          '0%': { transform: 'translateY(-4.5rem) translateX(-5rem)'},
+          '100%': { transform: 'translateY(-3rem) translateX(-5rem)' },
+        },
+        moveDownShop: {
+          '0%': { transform: 'translateY(-2.5rem) translateX(-12rem)'},
+          '100%': { transform: 'translateY(-0rem) translateX(-12rem)' },
+        },
+        moveDownGift: {
+          '0%': { transform: 'translateY(-3rem) translateX(10rem)'},
+          '100%': { transform: 'translateY(-0rem) translateX(10rem)'},
+        },
+      },
+      animation: {
+        moveDownMail: 'moveDownMail 1.5s ease-out forwards',
+        moveDownShop: 'moveDownShop 2s ease-out forwards',
+        moveDownGift: 'moveDownGift 1s ease-out forwards',
+      },
+    },
   },
   plugins: [],
 }
