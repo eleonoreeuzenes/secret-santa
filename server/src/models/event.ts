8bf6bd3393d8c2c65448eaa224c6fbd5402d726c
Eleonore Euzenes <eleonore.euzenes@gmail.com>
2024-12-12 13:35:34 +0100

    change date to string

--- server/src/models/event.ts
+++ server/src/models/event.ts
@@ -4,7 +4,7 @@ import { EventInterface } from "../types/event.interface";
 const eventSchema = new Schema<EventInterface & Document>({
   organizer: { type: String, required: true },
   event_name: { type: String, required: true },
-  event_date: { type: Date, required: true },
+  event_date: { type: String, required: true },
   event_location: { type: String, required: true },
   budget: { type: Number, required: true },
   participants: {
